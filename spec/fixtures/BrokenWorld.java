da7bfc5ff80ca520b8675bfb65ea1b55bf34c4b2
Florian Breisch <florian.breisch@mindkeeper.solutions>
2016-02-11 13:39:19 +0100

    add .java-fixtures for faulty, green and empty code

---
+++ spec/fixtures/BrokenWorld.java
@@ -0,0 +1,12 @@
+*
+  This world is realistic.
+*/
+public class BrokenWorld
+
+  @SuppressWarnings({})
+  public static void main (String[ args)
+  {
+    / This prints "Hello World!":
+    System.out.println("Hello World!")
+  }
+}
